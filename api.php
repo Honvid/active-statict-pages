672e3bf3be25aad130ceb9d99cf8a47edf7ae6e3
koran12@126.com <koran12@126.com>
2016-12-12 11:57:12 +0800

    add some assets.

--- api.php
+++ api.php
@@ -1,18 +1,22 @@
 <?php
 require 'require.php';
 
-if(!empty($_GET['type']) && $_GET['type'] != 'Base'){
+if(!empty($_GET['type'])){
     $data = JsonHelper::read($_GET['type']);
-    echo XmlHelper::response($data);
-    exit;
-}else{
-    $data = JsonHelper::read('Base');
-    $temp = [];
-    foreach ($data as $key => $value) {
-        if($value['status']) {
-            $temp[$key] = $value;
+    if($_GET['type'] == 'Base') {
+        $temp = [];
+        foreach ($data as $key => $value) {
+            if($value['status']) {
+                $temp[$key] = $value;
+            }
+        }
+        echo XmlHelper::responseBase($temp);
+    }else{
+        if($_GET['type'] == 'Persons') {
+            echo $data[0];
+        }else {
+            echo XmlHelper::response($data);
         }
     }
-    echo XmlHelper::responseBase($temp);
-    exit;
 }
+exit;
--- save.php
+++ save.php
@@ -1,15 +1,16 @@
 <?php
 require 'require.php';
 
-if(!empty($_POST['type']) && isset($_POST['key']) && !empty($_POST['name'])) {
-    if(in_array($_POST['type'], ['BigData', 'Brain', 'Cloud', 'Hat', 'Human'])) {
+if(!empty($_POST['type'])) {
+    $type = $_POST['type'];
+    if(in_array($_POST['type'], ['BigData', 'Brain', 'Cloud', 'Hat', 'Human'])
+        && isset($_POST['key']) && !empty($_POST['name'])) {
         JsonHelper::save(intval($_POST['key']), $_POST['name'], $_POST['type']);
-        exit;
-    }
-}
-
-if(!empty($_POST['type']) && isset($_POST['key']) && !empty($_POST['title']) && !empty($_POST['num'])) {
-    if($_POST['type'] == 'Base') {
+    }else if ($type == 'Base' && isset($_POST['key']) && !empty($_POST['title']) && !empty($_POST['num'])) {
         JsonHelper::saveBase(intval($_POST['key']), $_POST['title'], intval($_POST['num']), 0, intval($_POST['status']), $_POST['type']);
+    }else if ($type == 'Persons' && !empty($_POST['num'])) {
+
+        JsonHelper::write([$_POST['num']], 'Persons');
     }
 }
+exit;
