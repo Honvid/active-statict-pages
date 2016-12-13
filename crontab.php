c213618072b11a712c8296dafd84fafe6b87c1b6
koran12@126.com <koran12@126.com>
2016-12-13 17:44:15 +0800

    update the styles for the theme.

--- crontab.php
+++ crontab.php
@@ -10,11 +10,11 @@ set_time_limit(0);
 
 require 'helper/JsonHelper.php';
 
-$seven = strtotime('2016-12-13 17:40');
-$eight = strtotime('2016-12-13 18:10');
-$nine = strtotime('2016-12-13 19:10');
-$twelve = strtotime('2016-12-13 22:10');
-$after = strtotime('2016-12-14 3:10');
+$seven = strtotime('2016-12-13 17:50');
+$eight = strtotime('2016-12-13 18:20');
+$nine = strtotime('2016-12-13 19:20');
+$twelve = strtotime('2016-12-13 22:20');
+$after = strtotime('2016-12-14 3:20');
 
 // 在早上07：30的时候云计算 大数据 软件定义 融合架构  200起 但是要按顺序来
 // 8：00时候 云计算 达到500 左右
@@ -87,7 +87,7 @@ function countNumber($current, $total, $key, $now, $task, $i)
     $quan = round(10 / ($key + 1));
     $result = $total - $current;
     if($result > 0) {
-        return $current + round($result / ( ($task - $now) / 60 / (10 - $i)) );
+        return $current + round($result / ( ($task - $now) / 600) );
     }else{
         return $current + $quan;
     }
